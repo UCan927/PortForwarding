939ff0442ca2cbabc7f41d0a1bc7d820419908f7
UCan927 <UCan927@qq.com>
2018-11-09 01:07:52 +0800

    对SeesionContext进行重构;

--- Source Code/UCanSoft.PortForwarding.Tcp2Udp/Core/ConnectorHandler.cs
+++ Source Code/UCanSoft.PortForwarding.Tcp2Udp/Core/ConnectorHandler.cs
@@ -1,11 +1,6 @@
-﻿using Mina.Core.Buffer;
-using Mina.Core.Service;
+﻿using Mina.Core.Service;
 using Mina.Core.Session;
 using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using UCanSoft.PortForwarding.Common.Codec.Datagram;
 using UCanSoft.PortForwarding.Common.Utility.Helper;
 
@@ -13,183 +8,6 @@ namespace UCanSoft.PortForwarding.Tcp2Udp.Core
 {
     class ConnectorHandler : IoHandlerAdapter, ISingleInstance
     {
-        public class Context
-        {
-            private readonly CancellationTokenSource _cts = new CancellationTokenSource();
-            private readonly ConcurrentQueue<Int64> datagramQueue = new ConcurrentQueue<Int64>();
-            private readonly ConcurrentDictionary<Int64, DatagramModel> datagrams = new ConcurrentDictionary<Int64, DatagramModel>();
-            private readonly ConcurrentQueue<Int64> synAckQueue = new ConcurrentQueue<Int64>();
-            private readonly ConcurrentDictionary<Int64, DatagramModel> synAcks = new ConcurrentDictionary<Int64, DatagramModel>();
-            private readonly ConcurrentDictionary<Int64, DatagramModel> datagramsUseModelId = new ConcurrentDictionary<Int64, DatagramModel>();
-            private readonly ConcurrentDictionary<Int64, DatagramModel> datagramsUseSynAckId = new ConcurrentDictionary<Int64, DatagramModel>();
-            private readonly IoSession _session = null;
-            private readonly ManualResetEventSlim _mEvt = new ManualResetEventSlim(false);
-            private readonly SpinWait _spin = new SpinWait();
-            private Int64 _idGenerator = 0L;
-            private Task _sendDatagramTask;
-            private Task _sendSynAckTask;
-            private Int32 _isSendingSynAck = 0;
-            private Int32 _isSendingDatagram = 0;
-
-            public Context(IoSession session)
-            {
-                _session = session;
-            }
-
-            public void Enqueue(ArraySegment<Byte> bytes)
-            {
-                foreach (var buffer in Slice(bytes))
-                {
-                    var id = GenerateId();
-                    var model = DatagramModel.Create(id, bytes.Array);
-                    datagramQueue.Enqueue(id);
-                    datagrams.TryAdd(id, model);
-                }
-                if (Interlocked.CompareExchange(ref _isSendingDatagram, 1, 0) == 1)
-                    return;
-                var token = _cts.Token;
-                _sendDatagramTask = Task.Delay(TimeSpan.FromSeconds(1.0D), token)
-                                        .ContinueWith((t) => SendDatagram(token));
-            }
-
-            public void HandleSYN(DatagramModel model)
-            {
-                if (model.Type != DatagramModel.DatagramTypeEnum.SYN)
-                    return;
-                if (datagramsUseModelId.ContainsKey(model.Id))
-                    return;
-                var id = GenerateId();
-                var synAckModel = DatagramModel.Create(id, model.Id, DatagramModel.DatagramTypeEnum.SYNACK);
-                synAckQueue.Enqueue(id);
-                synAcks.TryAdd(id, synAckModel);
-                datagramsUseModelId.TryAdd(model.Id, model);
-                datagramsUseSynAckId.TryAdd(id, model);
-                if (Interlocked.CompareExchange(ref _isSendingSynAck, 1, 0) == 1)
-                    return;
-                var token = _cts.Token;
-                _sendSynAckTask = Task.Delay(TimeSpan.FromSeconds(1.0D), token)
-                                      .ContinueWith((t) => SendSynAck(token));
-            }
-
-            public void HandleSynAck(DatagramModel model)
-            {
-                if (model.Type != DatagramModel.DatagramTypeEnum.SYNACK)
-                    return;
-                var id = GenerateId();
-                var ackModel = DatagramModel.Create(id, model.Id, DatagramModel.DatagramTypeEnum.ACK);
-                _session.Write(ackModel);
-                if (!model.TryGetAckId(out Int64 modelAckId))
-                    throw new FormatException("ACK数据包格式不正确.");
-                if (!datagrams.ContainsKey(modelAckId))
-                    return;
-                if (datagramQueue.TryDequeue(out Int64 datagramId)
-                    && datagramId != modelAckId)
-                    throw new FormatException("ACK数据包Id与Datagram队列不匹配.");
-                datagrams.TryRemove(datagramId, out DatagramModel datagram);
-            }
-
-            public void HandleACK(DatagramModel model)
-            {
-                if (model.Type != DatagramModel.DatagramTypeEnum.ACK)
-                    return;
-                if (!model.TryGetAckId(out Int64 modelAckId))
-                    throw new FormatException("ACK数据包格式不正确.");
-                if (synAckQueue.TryDequeue(out Int64 ackId)
-                    && ackId != modelAckId)
-                    throw new FormatException("ACK数据包Id与SynACK队列不匹配.");
-                synAcks.TryRemove(ackId, out DatagramModel synAckModel);
-                if (datagramsUseSynAckId.TryRemove(ackId, out DatagramModel datagram)
-                    && datagram != null)
-                {
-                    datagramsUseModelId.TryRemove(datagram.Id, out DatagramModel tmpDatagram);
-                    var pipeSession = _session?.GetAttribute<IoSession>(SingleInstanceHelper<ConnectorHandler>.Instance.PipelineSessionKey);
-                    if (pipeSession == null)
-                        return;
-                    var slice = datagram.ToIoBuffer().GetSlice(DatagramModel.HeaderLength, datagram.DatagramLength);
-                    pipeSession.Write(slice.GetRemaining());
-                }
-            }
-
-            private void SendDatagram(CancellationToken token)
-            {
-                _sendDatagramTask = _sendDatagramTask.ContinueWith((t) => {
-                    TimeSpan cooldown = TimeSpan.Zero;
-                    try
-                    {
-                        if (!datagramQueue.TryPeek(out Int64 id))
-                            return;
-                        if (!datagrams.TryGetValue(id, out DatagramModel model)
-                            || model == null)
-                            return;
-                        cooldown = model.Cooldown;
-                        if (cooldown != TimeSpan.Zero)
-                            return;
-                        _session.Write(model);
-                    }
-                    finally
-                    {
-                        if (cooldown != TimeSpan.Zero)
-                            _mEvt.Wait(cooldown, token);
-                        else
-                            _spin.SpinOnce();
-                        SendDatagram(token);
-                    }
-                }, token);
-            }
-
-            private void SendSynAck(CancellationToken token)
-            {
-                _sendSynAckTask = _sendSynAckTask.ContinueWith((t) =>
-                {
-                    TimeSpan cooldown = TimeSpan.Zero;
-                    try
-                    {
-                        if (!synAckQueue.TryPeek(out Int64 id))
-                            return;
-                        if (!synAcks.TryGetValue(id, out DatagramModel model)
-                            || model == null)
-                            return;
-                        cooldown = model.Cooldown;
-                        if (cooldown != TimeSpan.Zero)
-                            return;
-                        _session.Write(model);
-                    }
-                    finally
-                    {
-                        if (cooldown != TimeSpan.Zero)
-                            _mEvt.Wait(cooldown, token);
-                        else
-                            _spin.SpinOnce();
-                        SendSynAck(token);
-                    }
-                }, token);
-            }
-
-            private IEnumerable<Byte[]> Slice(ArraySegment<Byte> bytes)
-            {
-                if (bytes.Count <= DatagramModel.MaxDatagramLength)
-                    yield return bytes.Array;
-                else
-                {
-                    Byte[] retVal = new Byte[DatagramModel.MaxDatagramLength];
-                    var buffer = IoBuffer.Wrap(bytes.Array);
-                    while (buffer.HasRemaining)
-                    {
-                        var remaining = buffer.Remaining;
-                        if (remaining <= DatagramModel.MaxDatagramLength)
-                            retVal = new Byte[remaining];
-                        buffer.Get(retVal, 0, retVal.Length);
-                        yield return retVal;
-                    }
-                }
-            }
-
-            private Int64 GenerateId()
-            {
-                return Interlocked.Increment(ref _idGenerator);
-            }
-        }
-
         private readonly NLog.ILogger _logger = NLog.LogManager.GetLogger(typeof(ConnectorHandler).FullName);
         public AttributeKey PipelineSessionKey { get; } = new AttributeKey(typeof(ConnectorHandler), "PipelineSessionKey");
         public AttributeKey ContextKey { get; } = new AttributeKey(typeof(ConnectorHandler), "ContextKey");
@@ -238,13 +56,13 @@ namespace UCanSoft.PortForwarding.Tcp2Udp.Core
                          , cause);
         }
 
-        public Context GetSessionContext(IoSession session)
+        public SessionContext GetSessionContext(IoSession session)
         {
             AttributeKey key = ContextKey;
-            var retVal = session?.GetAttribute<Context>(key);
+            var retVal = session?.GetAttribute<SessionContext>(key);
             if (retVal == null)
             {
-                retVal = new Context(session);
+                retVal = new SessionContext(session, PipelineSessionKey);
                 session?.SetAttribute(key, retVal);
             }
             return retVal;
